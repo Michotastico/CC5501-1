26c2f23a192ae56cba78ff77e479d5d9629f1fad
Micho Llorens <mllorens@dcc.uchile.cl>
2015-07-02 23:49:31 -0300

    TriangularFace class created.

---
+++ tools/structures/TriangularFace.py
@@ -0,0 +1,24 @@
+__author__ = 'Michel Llorens'
+__email__ = "mllorens@dcc.uchile.cl"
+from tools.algorithm import MathMethods
+
+class TriangularFace:
+
+    def __init__(self, va, vb, vc):
+        self.vertexA = va
+        self.vertexB = vb
+        self.vertexC = vc
+        self.plane = MathMethods.calculate_triangular_plane(va, vb, vc)
+        self.points = list()
+        return
+
+    def add_point(self, point):
+        distance = MathMethods.point_plane_distance_no_abs(point, self.plane)
+        if distance > 0:
+            self.points.append(point)
+        return
+
+    def invert_normal(self):
+        self.plane = MathMethods.calculate_triangular_plane(self.vertexC, self.vertexB, self.vertexA)
+        return
+
