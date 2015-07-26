42e253e191f912c5ece4c87db6670994825ecb2d
Michel Llorens <mllorens@dcc.uchile.cl>
2015-07-26 17:35:03 -0300

    Example of visualization

---
+++ view/example.py
@@ -0,0 +1,56 @@
+__author__ = 'Michel Llorens'
+__email__ = "mllorens@dcc.uchile.cl"
+
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+from tools.algorithm.QuickHull import QuickHull
+
+cloud = list()
+cloud.append((1, 0, 0))
+cloud.append((-1, 0, 0))
+cloud.append((0, 1, 0))
+cloud.append((0, -1, 0))
+cloud.append((0, 0, 1))
+cloud.append((0, 0, -1))
+
+quick_hull = QuickHull(cloud)
+quick_hull.run()
+faces = quick_hull.get_faces()
+
+
+def figure():
+    glBegin(OpenGL.GL.GL_LINES)
+    for f in faces:
+        glVertex3fv(f.a())
+        glVertex3fv(f.b())
+        glVertex3fv(f.c())
+    glEnd()
+
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(OpenGL.GL.GL_COLOR_BUFFER_BIT | OpenGL.GL.GL_DEPTH_BUFFER_BIT)
+        figure()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
