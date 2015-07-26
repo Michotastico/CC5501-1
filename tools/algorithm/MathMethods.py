779ac1f843ea118bf1000f8c3dc401140be478cd
Michel Llorens <mllorens@dcc.uchile.cl>
2015-07-26 17:33:49 -0300

    Format of code

--- tools/algorithm/MathMethods.py
+++ tools/algorithm/MathMethods.py
@@ -4,20 +4,24 @@ from operator import itemgetter
 import math
 import numpy as np
 
+
 def obtain_extremes(cloud):
     x = (min(cloud, key=itemgetter(0)), max(cloud, key=itemgetter(0)))
     y = (min(cloud, key=itemgetter(1)), max(cloud, key=itemgetter(1)))
     z = (min(cloud, key=itemgetter(2)), max(cloud, key=itemgetter(2)))
     return [x, y, z]
 
+
 def delete_tuple(dots, extremes):
     delete_point(dots, extremes[0])
     delete_point(dots, extremes[1])
     return
 
+
 def delete_point(dots, point):
     dots.remove(point)
     return
+
 
 def euclidean_distance(a, b):
     distance = math.sqrt(math.pow((a[0] - b[0]), 2) +
@@ -25,11 +29,13 @@ def euclidean_distance(a, b):
                          math.pow((a[2] - b[2]), 2))
     return distance
 
+
 def first_segment(extremes):
     segment = max({euclidean_distance(extremes[0][0], extremes[0][1]): extremes[0]},
                   {euclidean_distance(extremes[1][0], extremes[1][1]): extremes[1]},
                    {euclidean_distance(extremes[2][0], extremes[2][1]): extremes[2]})
     return segment.values()[0]
+
 
 def farthest_point(segment, cloud):
     m, s = get_line_vector(segment[0], segment[1])
@@ -42,6 +48,7 @@ def farthest_point(segment, cloud):
             point = p
     return point
 
+
 def farthest_point_plane(plane, cloud):
     distance = 0
     point = None
@@ -51,6 +58,7 @@ def farthest_point_plane(plane, cloud):
             distance = d
             point = p
     return point
+
 
 def get_line_vector(a, b):
     M = (a[0], a[1], a[2])
@@ -63,6 +71,7 @@ def distance_line_point(m, s, p):
     value = np.linalg.norm(dot)/np.linalg.norm(s)
     return value
 
+
 def calculate_triangular_plane(a, b, c):
     ba = (a[0] - b[0], a[1] - b[1], a[2] - b[2])
     bc = (c[0] - b[0], c[1] - b[1], c[2] - b[2])
@@ -71,9 +80,11 @@ def calculate_triangular_plane(a, b, c):
     plane = (cross[0], cross[1], cross[2], d)
     return plane
 
+
 def point_plane_distance(point, plane):
     distance = np.absolute(point_plane_distance_no_abs(point, plane))
     return distance
+
 
 def point_plane_distance_no_abs(point, plane):
     upper = (plane[0] * point[0] + plane[1] * point[1] + plane[2] * point[2] + plane[3])
