a3e8c9018a6c1869759b1b47477632a94833c60d
Micho Llorens <mllorens@dcc.uchile.cl>
2015-07-26 22:12:38 -0300

    Fixed problems with comparison of edges.

--- tools/structures/Edge.py
+++ tools/structures/Edge.py
@@ -9,11 +9,25 @@ class Edge:
         self.vb = vertex_b
 
     def is_equals(self, other_edge):
-        boolean_va1 = self.va[0] == other_edge.va[0] & self.va[1] == other_edge.va[1] & self.va[2] == other_edge.va[2]
-        boolean_va2 = self.va[0] == other_edge.vb[0] & self.va[1] == other_edge.vb[1] & self.va[2] == other_edge.vb[2]
+        boolean_va11 = self.va[0] == other_edge.va[0]
+        boolean_va12 = self.va[1] == other_edge.va[1]
+        boolean_va13 = self.va[2] == other_edge.va[2]
+        boolean_va1 = boolean_va11 & boolean_va12 & boolean_va13
 
-        boolean_vb1 = self.vb[0] == other_edge.va[0] & self.vb[1] == other_edge.va[1] & self.vb[2] == other_edge.va[2]
-        boolean_vb2 = self.vb[0] == other_edge.vb[0] & self.vb[1] == other_edge.vb[1] & self.vb[2] == other_edge.vb[2]
+        boolean_va21 = self.va[0] == other_edge.vb[0]
+        boolean_va22 = self.va[1] == other_edge.vb[1]
+        boolean_va23 = self.va[2] == other_edge.vb[2]
+        boolean_va2 = boolean_va21 & boolean_va22 & boolean_va23
+
+        boolean_vb11 = self.vb[0] == other_edge.va[0]
+        boolean_vb12 = self.vb[1] == other_edge.va[1]
+        boolean_vb13 = self.vb[2] == other_edge.va[2]
+        boolean_vb1 = boolean_vb11 & boolean_vb12 & boolean_vb13
+
+        boolean_vb21 = self.vb[0] == other_edge.vb[0]
+        boolean_vb22 = self.vb[1] == other_edge.vb[1]
+        boolean_vb23 = self.vb[2] == other_edge.vb[2]
+        boolean_vb2 = boolean_vb21 & boolean_vb22 & boolean_vb23
 
         boolean_va = boolean_va1 | boolean_va2
         boolean_vb = boolean_vb1 | boolean_vb2
